69aa17d99ef5147e23907ad51261dd858a713ffa
rawlins-t <rawlins@intros.at>
2021-03-27 18:33:40 +0000

    Using correct zone.

--- terraform-deploy/www-1.tf
+++ terraform-deploy/www-1.tf
@@ -1,7 +1,7 @@
 resource "digitalocean_droplet" "www-1" {
   image = "ubuntu-18-04-x64"
   name = "www-1"
-  region = "nyc2"
+  region = "fra1"
   size = "s-1vcpu-1gb"
   private_networking = true
   ssh_keys = null
