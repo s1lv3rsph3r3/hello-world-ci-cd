e62f05a2e3843f23ef282116e360d385a4dfe67f
s1lv3rsph3r3 <tim.a.rawlins@gmail.com>
2021-04-27 23:21:44 +0100

    Include cloudflare DNS updates.

--- terraform-deploy/provider.tf
+++ terraform-deploy/provider.tf
@@ -24,3 +24,48 @@ provider "digitalocean" {
 data "digitalocean_ssh_key" "enigma-ssh" {
   name = "enigma-ssh"
 }
+
+provider "cloudflare" {
+  email = "tim.a.rawlins@gmail.com"
+  token = var.cloudflare_token
+}
+
+variable "domain" {
+  default = "packetpigeon.com"
+}
+
+resource "cloudflare_record" "packetpigeon" {
+  domain = var.domain
+  name   = "packetpigeon.com"
+  value  = digitalocean_droplet.www-1.ipv4_address
+  type = "A"
+  proxied = true
+  zone_id = var.cloudflare_zone_id
+}
+
+resource "cloudflare_record" "www" {
+  domain  = var.domain
+  name    = "www"
+  value   = digitalocean_droplet.www-1.ipv4_address
+  type    = "A"
+  proxied = true
+  zone_id = var.cloudflare_zone_id
+}
+
+resource "cloudflare_record" "api" {
+  domain = var.domain
+  name   = "api"
+  value  = digitalocean_droplet.www-1.ipv4_address
+  type = "A"
+  proxied = true
+  zone_id = var.cloudflare_zone_id
+}
+
+resource "cloudflare_record" "service" {
+  domain = var.domain
+  name   = "service"
+  value  = digitalocean_droplet.www-1.ipv4_address
+  type = "A"
+  proxied = true
+  zone_id = var.cloudflare_zone_id
+}
