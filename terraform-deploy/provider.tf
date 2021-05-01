60e23831cc8fbde8d426a27b5155d5eb8126b38e
s1lv3rsph3r3 <tim.a.rawlins@gmail.com>
2021-05-01 19:20:30 +0100

    Add terraform for AWS bucket creation.

--- terraform-deploy/provider.tf
+++ terraform-deploy/provider.tf
@@ -8,6 +8,10 @@ terraform {
       source = "cloudflare/cloudflare"
       version = "~> 2.0"
     }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
   }
 }
 
@@ -16,9 +20,18 @@ variable "pvt_key" {}
 variable "pvt_key_file" {}
 variable "cloudflare_token" {}
 variable "cloudflare_zone_id" {}
+variable "aws_region" {}
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
 
 provider "digitalocean" {
   token = var.do_token
+}
+
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 data "digitalocean_ssh_key" "enigma-ssh" {
@@ -32,6 +45,13 @@ provider "cloudflare" {
 
 variable "domain" {
   default = "packetpigeon.com"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "packet-pigeon-console-terraform-state"
+  versioning {
+    enabled = true
+  }
 }
 
 resource "cloudflare_record" "packetpigeon" {
@@ -65,3 +85,11 @@ resource "cloudflare_record" "service" {
   proxied = true
   zone_id = var.cloudflare_zone_id
 }
+
+//terraform {
+//  backend "s3" {
+//    bucket = "packet-pigeon-console-terraform-state"
+//    key = "default-infrastructure"
+//    region = var.aws_region
+//  }
+//}
